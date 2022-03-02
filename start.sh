35c2db91edd0261ed9fd535adef2b8495e193bea
shixiuhai <64567345+shixiuhai@users.noreply.github.com>
2022-03-02 23:19:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
+## 这个脚本只对linux有效，手机上需要安装termux windows电脑上可以写dos脚本或者将项目运行到linux子系统上
 #!/bin/bash
 while true
 do
   sleep 15
-  ping -c5 -q 114.114.114.114 # -c 5 是ping 5次的意思，-q 是隐藏ping的结果
+  ping -c 5 -q 114.114.114.114 # -c 5 是ping 5次的意思，-q 是隐藏ping的结果
   if [ $? -eq 0 ];then
-    echo "---校园网在现中---"
+    echo "---校园网在线中---"
    else
     python3 jxnu_wifi.py
 done
