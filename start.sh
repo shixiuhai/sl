8ef91ceb724f7525a41b05a8c0898fd08c4a7f7e
shixiuhai <64567345+shixiuhai@users.noreply.github.com>
2022-03-03 09:06:30 +0800

    Update start.sh
    校园网自动认证linux启动脚本

--- start.sh
+++ start.sh
@@ -2,10 +2,17 @@
 #!/bin/bash
 while true
 do
+  # 每隔15秒钟判断一次校园网是否在线
   sleep 15
   ping -c 5 -q 114.114.114.114 # -c 5 是ping 5次的意思，-q 是隐藏ping的结果
+  # 当有网络的时候判定校园网在线
   if [ $? -eq 0 ];then
     echo "---校园网在线中---"
-   else
+  # 在晚上0点到早上6点间，校园网认证功能关闭
+  elif [ $hh -ge 00 ] && [ $hh -le 06 ];then
+    echo "---现在是休息时间---"
+  # 在需要认证的时间内运行自当认证的python脚本
+  else
     python3 jxnu_wifi.py
+  fi
 done
